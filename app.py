761e48ae14f0b8d5ad7e006412d0ad28b7f1ef04
Dibya Das <dibyadascool@gmail.com>
2017-07-07 03:01:45 +0530

    heroku init

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def upload():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 9090))
-    app.run(host='localhost', port=port,debug=True)
+    app.run(host='0.0.0.0', port=port,debug=True)
