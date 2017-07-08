e9db5e8a340b1c2d659d23e44954f32ff6bda4f8
Dibya Das <dibyadascool@gmail.com>
2017-07-08 15:20:32 +0530

    asaving working changes

--- app.py
+++ app.py
@@ -13,17 +13,13 @@ def default_view():
 		{"id" : "1" , "group" : 1},
 		{"id" : "2" , "group" : 1},
 		{"id" : "3" , "group" : 1}
-		# {id : "4" , group : 1},
-		# {id : "5" , group : 1}
 	]
 	json_link = [
 		{"target": "2", "strength": 1,"source": "1"},
     	{"source": "2", "target": "3", "strength": 8}
-    	# {source: "3", target: "9", strength: 10},
-   	 # 	{source: "4", target: "8", strength: 6},
-    	# {source: "5", target: "7", strength: 1}
 	]
-	return render_template('for.html',json_link = json_link,json_node =json_node,text = '')
+	graph_data = info(json_node,json_link)
+	return render_template('for.html',json_link = json_link,json_node =json_node, graph_data=graph_data)
 
 @app.route('/preset',methods=['POST'])
 def preset():
@@ -33,7 +29,8 @@ def preset():
 		json_node = pickle.load(f)
 	with open('presets/'+text+'/'+flag+'detected_'+text+'_'+'links','rb') as f:
 		json_link = pickle.load(f)
-	return render_template('for.html',json_link=json_link,json_node=json_node,text=text)
+	graph_data = info(json_node,json_link)
+	return render_template('for.html',json_link = json_link,json_node =json_node, graph_data=graph_data)
 
 
 @app.route('/detect',methods=['POST'])
@@ -64,7 +61,8 @@ def detect(na=None,la=None):
 	for i in la:
 		json_link.append({'source':i['source']['id'],'target':i['target']['id'],'strength':1})
 
-	return render_template('for.html',json_link=json_link,json_node=json_node)
+	graph_data = info(json_node,json_link)
+	return render_template('for.html',json_link = json_link,json_node =json_node, graph_data=graph_data)
 
 @app.route('/upload',methods=['POST'])
 def upload():
@@ -79,17 +77,46 @@ def upload():
 		edgelist = t.split('\n')
 		for i in t.replace('\n',' ').split(' '):
 			if i not in nodes:
-				nodes.append({'id':i})
+				nodes.append(i)
+
+		json_node = []
+		json_link = []
+
+		for i in nodes:
+			json_node.append({'id':i,'group':1})
 
 		for i in edgelist:
 			s = i.split(' ')
-			edges.append({'source':{'id':s[0]},'target':{'id':s[1]}})
+			# edges.append({'source':{'id':s[0]},'target':{'id':s[1]}})
+			edges.append({'source':s[0],'target':s[1],'strength': 1})
 
-		return detect(nodes,edges)
+		# return detect(nodes,edges)
+		graph_data = info(json_node,json_link)
+		return render_template('for.html',json_link = json_link,json_node =json_node, graph_data=graph_data)
 	except Exception as e:
 		raise e from None
 		# print(e)
 		return "Unsuccessful"
+
+def info(json_node,json_link):
+	nodes = []
+	edges = []
+	G = nx.Graph()
+
+	for i in json_node:
+		nodes.append(i['id'])
+
+	for i in json_link:
+		edges.append((i['source'],i['target']))
+
+	G.add_nodes_from(nodes)
+	G.add_edges_from(edges)
+	degree_centrality = nx.degree_centrality(G)
+	betweenness_centrality = nx.betweenness_centrality(G)
+	clustering_coefficent = nx.clustering(G)
+	return [degree_centrality,betweenness_centrality,clustering_coefficent]
+
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 9090))
